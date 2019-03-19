1767dd016db6503ae87082f2b6658c15ece94e78
Milad Mobarhan <milad.mobarhan@cognite.com>
2019-03-19 13:16:34 +0100

    Updates

---
+++ precache-manifest.d3026b793b1a4e3ce2502eebe9ca752a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4843ea6350aa13ece06a456a6fb9564",
+    "url": "/scanify/static/media/help.d4843ea6.png"
+  },
+  {
+    "revision": "a6d846586291a95d6788",
+    "url": "/scanify/static/js/runtime~main.a6d84658.js"
+  },
+  {
+    "revision": "d3949d3c62e9200e3677",
+    "url": "/scanify/static/js/main.d3949d3c.chunk.js"
+  },
+  {
+    "revision": "813c4c0ee115bf46aabd",
+    "url": "/scanify/static/js/2.813c4c0e.chunk.js"
+  },
+  {
+    "revision": "d3949d3c62e9200e3677",
+    "url": "/scanify/static/css/main.55cf019f.chunk.css"
+  },
+  {
+    "revision": "d5245903d489240dd1973d27fc3147a2",
+    "url": "/scanify/index.html"
+  }
+];
